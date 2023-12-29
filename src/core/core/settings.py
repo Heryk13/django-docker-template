eda9eec99a4b0a488c6fa47e6fd5e6f417e1278e
Heryk13 <otakiheryk@gmail.com>
2023-12-30 03:20:45 +0900

    static url hotFix

--- src/core/core/settings.py
+++ src/core/core/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATICFILES_LOCATION = 'static'
 STATIC_URL = 'static/'
-# STATIC_ROOT = 'static'
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
